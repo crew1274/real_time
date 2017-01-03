c892b761ddb330f5b14c0db395a2311d5e9939ba
crew1274 <crew1274@yahoo.com.tw>
2017-01-04 05:02:48 +0800

    update network config

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,7 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Demand_record;
+
 class DashboardController extends Controller
 {
-    //
+    public function __construct()
+    {
+     $this->middleware('auth');
+    }
+    public function index()
+    {
+        //$records = Demand_record::orderBy('updated_at','ASC')->limit(3)->get();
+        return view('home');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,15 +11,35 @@
 |
 */
 
+/*demo測試路徑*/
 Route::group(['prefix' => 'demo'], function () {
     Route::get('real', 'RealController@real');
     Route::get('random', 'RealController@random');
     Route::get('/', 'RecordController@index');
 });
 
-Route::get('dashboard', 'DashboardController@index');
 Route::get('/', function(){return view('welcome'); });
+Route::get('dashboard', 'DashboardController@index');
+Route::get('ecord', 'RecordController@index');
+Route::resource('boot','SettingController');
+Route::resource('peaktime','ConfigController');
+
+Route::get('profile', 'ProfileController@index');
+Route::post('profile', 'ProfileController@update');
+
+/*網路設定*/
+Route::group(['prefix' => 'network'], function () {
+    Route::get('wifi', 'NetworkController@wifi');
+    Route::post('wifi', 'NetworkController@wifi_p');
+    Route::get('static', 'NetworkController@staticip');
+    Route::post('static', 'NetworkController@staticip_p');
+    Route::get('dhcp', 'NetworkController@dhcp');
+    Route::post('dhcp', 'NetworkController@dhcp_p');
+    Route::get('/', 'NetworkController@index');
+});
+
+/*更換語系*/
+Route::get('lang/{lang}', ['as'=>'lang.switch', 'uses'=>'LanguageController@switchLang']);
 
 Auth::routes();
-
 #Route::get('/home', 'HomeController@index');
