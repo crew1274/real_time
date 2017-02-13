b13eff3c1e61398546885c10429b2509f55401ab
crew1274 <crew1274@yahoo.com.tw>
2017-02-14 05:01:52 +0800

    update button

--- resources/views/control.blade.php
+++ resources/views/control.blade.php
@@ -2,5 +2,17 @@
 @section('title','Control')
 @section('page_heading','控制開關')
 @section('section')
+<div class="col-sm-6">
+		@section ('5button_panel_title','Device #1')
+		@section ('5button_panel_body')
+			<h4>Control Buttons</h4>
+			<p>
+            <a class="btn btn-info" href="{{ url('control/on')}}"><i class="fa fa-check"></i></a>
+	 	    <a class="btn btn-warning" href="{{ url('control/off')}}"><i class="fa fa-times"></i></a>
+			</p>
+			<br>
+		@endsection
+		@include('widgets.panel', array('as'=>'5button', 'header'=>true))
+	</div>
 
 @stop
