03e0b9294df4178a60722f455b1ec57aed2aa168
crew1274 <crew1274@yahoo.com.tw>
2017-02-20 16:28:22 +0800

    update python function

--- python/valid.py
+++ python/valid.py
@@ -1,27 +1,36 @@
-import sys
-import pymysql.cursors
+   import sys
+   import pymysql.cursors
+
+   sys.path.insert(0, '/home/pi/newcode')
+
+   import dae as DAE
+   import time
 
 # Connect to the database
 #print ('Number of arguments:', len(sys.argv), 'arguments.')
 #print ('Argument List:', str(sys.argv))
-connection = pymysql.connect(host='localhost',
-                             user='root',
-                             password='pi',
-                             db='dae',
-                             charset='utf8mb4',
-                             cursorclass=pymysql.cursors.DictCursor)
-
 try:
-    with connection.cursor() as cursor:
-        # Create a new record
-        sql = "UPDATE `dae`.`settings` SET `token` = '1' WHERE `settings`.`id` = %s;"
+   daesql = DAE.SQL()
+    #ser = DAE.ModbusSerial()
+  #settings = daesql.getSettings()
+   # meter circuit settings
+    #highCode = settings['highCode']
+   #lowCode = settings['lowCode']
+    #baudrate = settings['baudrate']
+   #obj = settings['obj']
+    #address = obj['address']
+   #ch = obj['ch']
+    #ser.writeMeterCircuit(highCode, lowCode, address, ch, baudrate, 1)
+ #time.sleep(2)
+   # test response
+  #response = ser.writeCommandToModbus([1,3,96,0,0,2])
+    #if response != []:
+     # valid
+ sql = "UPDATE `demand`.`settings` SET `token` = '1' WHERE `settings`.`id` = {0};".format(sys.argv[1])
 
-        cursor.execute(sql, (str(sys.argv[1])))
-
-    # connection is not autocommit by default. So you must commit to save
-    # your changes.
-    connection.commit()
-
+  daesql.executeSQL(sql)
+  print("id:",str(sys.argv[1]), "vaild Successfully!")
+  #else:
+    #    print("no response.")
 finally:
-    connection.close()
-    print("id:",str(sys.argv[1])," setting vaild!")
+  daesql.closeConnection()
