518037f55482a0e6cd840e9175b061749f75d7da
alikuotw <alikuotw@gmail.com>
2017-02-21 16:10:46 +0800

    update token = 1 when click verification

--- python/valid.py
+++ python/valid.py
@@ -1,17 +1,30 @@
-
-import sys
-if sys.platform != 'linux':
-  print("Not linux env.")
-  exit()
-else:
-  sys.path.insert(0, '/')
 import pymysql.cursors
 import time
-import dae as DAE
-try:
-  daesql = DAE.SQL()
-  sql = "UPDATE `demand`.`settings` SET `token` = '1' WHERE `settings`.`id` = {0};".format(sys.argv[1])
-  daesql.executeSQL(sql)
-  print("id:",str(sys.argv[1]), "vaild Successfully!")
-finally:
-  daesql.closeConnection()
+import sys
+
+if sys.platform != 'linux':
+    print("Not linux env.")
+    exit()
+
+else:
+    sys.path.insert(0, '/home/pi/workspace/DAESecondStage/')
+
+import DAE.MySQL as daesql
+import DAE.DBConfig as dbconfig
+
+
+def main():
+    global sqlObj
+    sqlObj = daesql.MySQL(dbconfig.mysql)
+
+    try:
+        sql = "UPDATE `demand`.`settings` SET `token` = '1' WHERE `settings`.`id` = {0};".format(sys.argv[1])
+        sqlObj.executeSQL(sql)
+        print("id:",str(sys.argv[1]), "vaild Successfully!")
+
+    finally:
+        sqlObj.close()
+
+
+if __name__ == '__main__':
+    main()
